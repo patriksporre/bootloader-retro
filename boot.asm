f4d77d1a0c453130c6a8b5331c6334795186e8f9
Patrik Sporre <patriksporre@gmail.com>
2024-10-29 15:27:19 +0100

    Adding disk retries, segment and stack setup by the book

--- boot.asm
+++ boot.asm
@@ -11,6 +11,15 @@ section .text               ; Code section
 start:
     mov [bootdrive], dl     ; Store the boot drive number from BIOS
 
+    ; Setup segments and stack
+    cli                     ; Disable interrupts
+    xor ax, ax              ; Zero out AX
+    mov ds, ax              ; Set DS (data segment) to 0x0000
+    mov es, ax              ; Set ES (extra segment) to 0x0000
+    mov ss, ax              ; Set SS (stack segment) to 0x0000
+    mov sp, 0x7c00          ; Set SP (stack pointer) to 0x7c00 (stack grows downwards)
+    sti                     ; Enable interrupts
+
 %ifdef DEBUG
     ; Display 'L' for loading stage
     mov ah, 0x0e            ; Teletype output
@@ -19,16 +28,36 @@ start:
 %endif
 
     ; Load the application from disk starting at sector 2 (1-based indexing)
-    mov ah, 0x02            ; Read disk sectors
-    mov al, SECTORS         ; Number of sectors to read
     mov ch, 0x00            ; Cylinder number
     mov cl, 0x02            ; Sector number (1-based indexing)
     mov dh, 0x00            ; Head number
     mov dl, [bootdrive]     ; Boot drive number (passed by BIOS)
     mov bx, LOAD_ADDR       ; Destination address in memory for the load
+
+    mov si, 3               ; BIOS disk read shall be given three attempts
+
+retry:
+    mov ah, 0x02            ; Read disk sectors
+    mov al, SECTORS         ; Number of sectors to read
     int 0x13                ; Disk BIOS services
 
-    jc  error               ; Jump to 'error' if reading failed
+    jnc disksuccess         ; Jump to 'success' if no reading error
+
+    dec si                  ; Decrement retry counter
+    jz  error               ; Jump to 'diskerror' if no more retries
+
+    mov ah, 0x00            ; Reset disk system
+    int 0x13                ; Disk BIOS services
+
+    jmp retry               ; Jump to 'retry'
+
+disksuccess:
+%ifdef DEBUG
+    ; Display 'S' for loading success
+    mov ah, 0x0e            ; Teletype output
+    mov al, 'S'             
+    int 0x10                ; Video BIOS services
+%endif
 
 %ifdef DEBUG
     ; Display 'J' for jumping stage
